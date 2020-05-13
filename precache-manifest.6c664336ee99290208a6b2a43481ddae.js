81f5b430fb31822fa676cf2e7c60a6810df3dd5f
Wang Zijun <wzj97@hotmail.com>
2020-05-14 01:12:02 +0800

    Updates

---
+++ precache-manifest.6c664336ee99290208a6b2a43481ddae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "731db9906532e8113a1e35c49149df06",
+    "url": "/teaching-indicators-demo/index.html"
+  },
+  {
+    "revision": "49c09050f9741b6efa12",
+    "url": "/teaching-indicators-demo/static/css/main.b758e6cf.chunk.css"
+  },
+  {
+    "revision": "13fc22968fbd72e4d4cc",
+    "url": "/teaching-indicators-demo/static/js/2.6669b0f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teaching-indicators-demo/static/js/2.6669b0f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c09050f9741b6efa12",
+    "url": "/teaching-indicators-demo/static/js/main.4604347b.chunk.js"
+  },
+  {
+    "revision": "eea2ce01638e9dfcebab",
+    "url": "/teaching-indicators-demo/static/js/runtime-main.879cea6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teaching-indicators-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
